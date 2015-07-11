5fe7343c656e6871b9ede86c6bd5cba6a96963c7
Christopher Bowles <christopherjbowles@gmail.com>
2015-07-11 21:22:13 +1000

    Add partial view for grid

---
+++ AsyncGrid/Helpers/AsyncTableHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace AsyncGrid.Helpers
+{
+    public static class AsyncTableHelper
+    {
+        public static IHtmlString TableHeaderLink(this HtmlHelper helper, string target, string caption, string name = null)
+        {
+            name = name ?? caption;
+            return helper.ActionLink(caption, target, new { SortBy = name, SortAsc = helper.ViewBag.SortDirections[name] });
+        }
+    }
+}
